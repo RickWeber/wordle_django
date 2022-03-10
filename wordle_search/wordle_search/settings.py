1018b739106a0a3e39ed4558a07a1b633bc26468
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-03-10 10:29:55 -0500

    hello world!

--- wordle_search/wordle_search/settings.py
+++ wordle_search/wordle_search/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'guess.apps.GuessConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
