5cd7f71c58c2c2cef027d7a01d9393a325d0dff5
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-03-11 09:14:21 -0500

    start fleshing out forms

---
+++ wordle_search/guess/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Guesses, TodaysAnswer
+from .wordlists import all_words
+
+class GuessForm(forms.ModelForm):
+    class Meta:
+        model = Guesses
+    pass
+
+# This should provide a text input
+# The form should validate the entry to make sure it's part 
+# of all_words.
+# Then it should run Guesses.next_guess() on the input, and
+# update the page to show that guess along with the associated
+# flags.
+
+
+class CheatForm(forms.ModelForm):
+    class Meta:
+        model = TodaysAnswer
